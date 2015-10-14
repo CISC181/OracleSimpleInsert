0d2e42e9574f17f5d12398c470676a9f1d10bce9
Bert.Gibbons <bgibbons@udel.edu>
2015-10-14 15:20:47 -0400

    Commit changes

--- OracleSimpleInsert/src/MainPackage/SimpleInsert.java
+++ OracleSimpleInsert/src/MainPackage/SimpleInsert.java
@@ -13,7 +13,7 @@ public class SimpleInsert {
             String password = "Section1";
             Connection conn = DriverManager.getConnection(url,username,password); 
             Statement st = conn.createStatement(); 
-            st.executeUpdate("INSERT INTO EMP " + 
+            st.executeUpdate("INSERT INTO Bert.EMP " + 
                 "(ID, First_Name, Last_Name, Salary, MiddleName)" +
                 "VALUES (seq_emp.nextval,'Bert','Jones',25000,'R')"); 
             System.out.println("Record Inserted");
